7e0b068d72de58a1ee403e9ab9b2f03650967916
Saidul Alam Sifat <saidulalamsifat00@gmail.com>
2025-02-28 23:00:30 +0600

    Add files via upload

---
+++ simple prblm.js
@@ -0,0 +1,9 @@
+let val = prompt("Enter the last digit of your id");
+let num = parseInt(val);
+console.log(num+1);
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
